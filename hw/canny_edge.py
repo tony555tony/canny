bc137495b231245636531b8b33c342482b7c4612
tony555tony <68364439+tony555tony@users.noreply.github.com>
2020-07-17 14:57:05 +0800

    Add files via upload

---
+++ hw/canny_edge.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('zzz.jpg', 0)
+edges = cv2.Canny(img, 50, 100)
+edges2 = cv2.Canny(img, 50, 150)
+edges3 = cv2.Canny(img, 50, 200)
+edges4 = cv2.Canny(img, 100, 150)
+edges5 = cv2.Canny(img, 185, 250)
+
+plt.subplot(231), plt.imshow(img, cmap='gray'), plt.title('Original Image1')
+plt.subplot(232), plt.imshow(edges, cmap='gray'), plt.title('Edge Image2')
+plt.subplot(233), plt.imshow(edges2, cmap='gray'), plt.title('Edge Image3')
+plt.subplot(234), plt.imshow(edges3, cmap='gray'), plt.title('Edge Image4')
+plt.subplot(235), plt.imshow(edges4, cmap='gray'), plt.title('Edge Image5')
+plt.subplot(236), plt.imshow(edges5, cmap='gray'), plt.title('Edge Image6')
+
+plt.show() 
+ 
+ 
